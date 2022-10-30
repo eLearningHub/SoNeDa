bfb88fbcfdb812894fdcdd83f00d4e077d044a17
Behzad Samadi <behzad.samadi@gmail.com>
2022-10-30 14:39:40 -0400

    twitter configure

--- src/sncli/services/twitter.py
+++ src/sncli/services/twitter.py
@@ -1,11 +1,15 @@
 import click
-
+import os
 
 @click.group()
 def twitter():
     pass
 
-
 @twitter.command()
-def zone():
-    click.echo('This is the zone subcommand of the twitter command')
+@click.option('--api-key', type=str, help='API key', default=lambda: os.environ.get("TWITTER_API_KEY", ""))
+@click.option('--api-secret-key', type=str, help='API secret key', default=lambda: os.environ.get("TWITTER_API_SECRET_KEY", ""))
+@click.option('--bearer-token', type=str, help='Bearer token', default=lambda: os.environ.get("TWITTER_BEARER_TOKEN", ""))
+@click.option('--access-token', type=str, help='Access token', default=lambda: os.environ.get("TWITTER_ACCESS_TOKEN", ""))
+@click.option('--access-token-secret', type=str, help='Access token secret', default=lambda: os.environ.get("TWITTER_ACCESS_TOKEN_SECRET", ""))
+def configure():
+    click.echo('Configure the Twitter account')
