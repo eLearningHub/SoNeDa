bd9f7375ce791f4290b8ccfbd2457c215f859339
Behzad Samadi <behzad.samadi@gmail.com>
2022-10-29 20:39:52 -0400

    Update

--- Makefile
+++ Makefile
@@ -6,3 +6,8 @@ conda-env-remove:
 
 requirements:
 	pip install -r requirements.txt
+
+poetry:
+	wget https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
+	python install-poetry.py
+	rm install-poetry.py
--- src/sncli/__main__.py
+++ src/sncli/__main__.py
@@ -1,16 +1,21 @@
 """Command-line interface."""
+from distutils.command.config import config
 import click
-import cli.app
 
-@cli.command()  # @cli, not @click!
-def sync():
-    click.echo('Syncing')
+@click.group()
+def twitter():
+    pass
+
+@twitter.command()  # @cli, not @click!
+def configure():
+    click.echo('Configuring')
+
+twitter.add_command(configure)
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """Social networks cli."""
-
 
 if __name__ == "__main__":
     main(prog_name="sncli")  # pragma: no cover
