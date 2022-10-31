3eb3025e268fc9868f00075307f5d065e7dd4f4b
Behzad Samadi <behzad.samadi@gmail.com>
2022-10-31 11:45:24 -0400

    Twitter profiles

--- src/sncli/functions/twitter.py
+++ src/sncli/functions/twitter.py
@@ -1,4 +1,10 @@
 import os
+import toml
 
 def get_twitter_credentials_file():
     return os.path.expanduser(os.getenv("TWITTER_CREDENTIALS_FILE", "~/.sncli/.twitter"))
+
+def twitter_profiles():
+    dot_twitter = get_twitter_credentials_file()
+    profiles = toml.load(dot_twitter)
+    return profiles
