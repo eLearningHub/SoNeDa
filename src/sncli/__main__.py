6cca6b7863ff73523f4869044f2c9fa2a95de3f6
Behzad Samadi <behzad.samadi@gmail.com>
2022-11-05 23:19:18 -0400

    simple

--- src/sncli/__main__.py
+++ src/sncli/__main__.py
@@ -1,9 +1,6 @@
 import sys
-import logging
 import click
 from sncli.services.twitter import twitter
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 @click.group()
 def cli():
--- src/sncli/logging_conf.py
+++ src/sncli/logging_conf.py
@@ -2,6 +2,7 @@ import logging
 import logging.config
 
 logging.config.fileConfig('sncli/logging.conf')
+logging.basicConfig(level=logging.INFO)
 
 # create logger
 logger = logging.getLogger('simple')
