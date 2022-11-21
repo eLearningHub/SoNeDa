8960c1f5ae5d9e16662b95cfaeae5c75cf8814a6
Behzad Samadi <behzad.samadi@gmail.com>
2022-11-21 00:53:59 -0500

    create_client_for_token

--- src/sncli/twitter/client.py
+++ src/sncli/twitter/client.py
@@ -8,6 +8,7 @@ import urllib
 import hashlib
 import oauthlib
 import requests
+from cryptography.fernet import Fernet
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 from oauthlib.oauth2.rfc6749.errors import MissingTokenError
@@ -20,7 +21,7 @@ class TwitterAPIClient:
 
     API_ROOT = os.environ.get("TWITTER_API_ROOT", "https://api.twitter.com")
     AUTH_URL = "https://twitter.com/i/oauth2/authorize"
-    TOKEN_URL = f"{API_ROOT}/2/oauth2/token"
+    TOKEN_URL = f"{API_ROOT}/oauth2/token"
     REDIRECT_URI = "http://127.0.0.1:5000/oauth/callback"
     API_ROOT = os.environ.get("TWITTER_API_ROOT", "https://api.twitter.com")
     KEY_DB = os.path.expanduser(os.getenv("TWITTER_KEY_DB", "~/.sncli/.twitter.key"))
@@ -34,6 +35,7 @@ class TwitterAPIClient:
 
         credentials = profiles[profile]
         self.consumer_key = credentials['consumer_key']
+        self.consumer_secret = credentials['consumer_secret']
 
         # Create a code verifier
         self.code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
@@ -44,23 +46,56 @@ class TwitterAPIClient:
         code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
         self.code_challenge = code_challenge.replace("=", "")
 
-        try:
+        print(self.KEY_DB)
+        if os.path.exists(self.KEY_DB):
             token = self.load_saved_token()
-        except KeyError:
+        else:
             token = self.fetch_api_token()
             self.token_saver(token)
+        
         self.client = self.create_client_for_token(token)
+
+
+    def fernet(self, key: str) -> Fernet:
+        key = key + "=" * (len(key) % 4)
+        _key = base64.urlsafe_b64encode(base64.urlsafe_b64decode(key))
+        return Fernet(_key)
+
+    def encrypt(self, data: str) -> bytes:
+        return self.fernet(self.consumer_secret).encrypt(data.encode())
+
+    def decrypt(self, data: bytes) -> bytes:
+        return self.fernet(self.consumer_secret).decrypt(data)
 
     def token_saver(self, token: dict) -> None:
         data = json.dumps(token)
+        print(data)
         _t = self.encrypt(data)
-        with dbm.open(self.KEY_DB.as_posix(), "c") as db:
+        with dbm.open(self.KEY_DB, "c") as db:
             db[self.consumer_key] = _t
 
     def load_saved_token(self) -> dict:
-        with dbm.open(self.KEY_DB.as_posix(), "c") as db:
-            _token = db[self.consumer_key]
-        token = json.loads(self.decrypt(_token))
+        try:
+          with dbm.open(self.KEY_DB, "c") as db:
+              _token = db[self.consumer_key]
+          token = json.loads(self.decrypt(_token))
+          return token
+        except:
+          raise(KeyError)
+
+    def fetch_api_token(self) -> dict:
+        backend = BackendApplicationClient(client_id=self.consumer_key)
+        oauth = OAuth2Session(client=backend)
+        try:
+            token = oauth.fetch_token(
+                token_url=self.TOKEN_URL,
+                client_id=self.consumer_key,
+                client_secret=self.consumer_secret
+            )
+        except MissingTokenError:
+            # oauthlib gives the same error regardless of the problem
+            print("Something went wrong, please check your client credentials.")
+            raise
         return token
 
     def create_client_for_token(self, token: dict) -> OAuth2Session:
@@ -71,22 +106,6 @@ class TwitterAPIClient:
             auto_refresh_kwargs={},
             token_updater=self.token_saver,
         )
-
-    def fetch_api_token(self) -> dict:
-        backend = BackendApplicationClient(client_id=self.consumer_key)
-        oauth = OAuth2Session(client=backend)
-        try:
-            token = oauth.fetch_token(
-                token_url=self.TOKEN_URL,
-                client_id=self.consumer_key,
-                client_secret=self.app_secret,
-                include_client_id=True,
-            )
-        except MissingTokenError:
-            # oauthlib gives the same error regardless of the problem
-            print("Something went wrong, please check your client credentials.")
-            raise
-        return token
 
     def clear_saved_token(self) -> None:
         with dbm.open(self.KEY_DB.as_posix(), "c") as db:
@@ -116,11 +135,7 @@ class TwitterAPIClient:
     def post(self, path:str, data:dict) -> requests.Response:
         url = self.api_url+path
         #logger.debug(f"POSTing URL {url}")
-        headers={
-            "Authorization": "Bearer {}".format(token["access_token"]),
-            "Content-Type": "application/json",
-        }
-        return self.client.post(url, json=data, headers=headers)
+        return self.client.post(url, json=data)
 
     def put(self, path:str, data:dict) -> requests.Response:
         url = f"{self.TWITTER_API_ROOT}{path}"
