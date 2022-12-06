16556976fdba9476b11f8f8c6d9742f419f1310a
Behzad Samadi <behzad.samadi@gmail.com>
2022-12-05 23:32:22 -0500

    tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,8 @@
 """Test cases for the __main__ module."""
 
-from soneda.cli import app
+from soneda.twitter.client import TwitterAPIClient
 
-
-def test_main_succeeds() -> None:
-    """It exits with a status code of zero."""
-    result = app()
-    assert result.exit_code == 0
+def test_tweets_lookup() -> None:
+    twitter = TwitterAPIClient()
+    tweet = twitter.get("/2/tweets", ids=1460323737035677698)
+    assert tweet[0]["id"] == "1460323737035677698"
