1532eb5af667df0f6fa58ebeee07e92c7b3aa89b
Behzad Samadi <behzad.samadi@gmail.com>
2022-12-11 00:18:11 -0500

    pre-commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,21 @@
 """Test cases for the __main__ module."""
 
 import os
+
 from soneda.twitter.app import config
 from soneda.twitter.client import TwitterAPIClient
 
 
 def test_tweets_lookup() -> None:
     """Tweets lookup test."""
-    config(os.getenv('TWITTER_PROFILE'), 
-           os.getenv('TWITTER_CONSUMER_KEY'), 
-           os.getenv('TWITTER_CONSUMER_SECRET'),
-           os.getenv('TWITTER_ACCESS_TOKEN_KEY'),
-           os.getenv('TWITTER_ACCESS_TOKEN_SECRET'),
-           os.getenv('TWITTER_BEARER_TOKEN'))
+    config(
+        os.getenv("TWITTER_PROFILE"),
+        os.getenv("TWITTER_CONSUMER_KEY"),
+        os.getenv("TWITTER_CONSUMER_SECRET"),
+        os.getenv("TWITTER_ACCESS_TOKEN_KEY"),
+        os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+        os.getenv("TWITTER_BEARER_TOKEN"),
+    )
     twitter = TwitterAPIClient()
     tweet = twitter.get("/2/tweets", ids=1460323737035677698)
     print(tweet)
